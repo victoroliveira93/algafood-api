f32945eb3daf7d8f4020d4740597000aeeaae85d
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-06-04 09:30:41 -0300

    first commit

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+	
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@GetMapping
+	public List<Restaurante> listar() {
+		return restauranteRepository.listar();
+	}
+
+}
