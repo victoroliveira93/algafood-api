866d4298affec178e66d708c293635ed95e98c3e
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-06-04 09:36:37 -0300

    GET Restaurante

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,18 @@ public class RestauranteController {
 	@GetMapping
 	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
+	}
+	
+	@GetMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+		
+		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
+		
+		if(restaurante != null) {
+			return ResponseEntity.ok(restaurante);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+}
